00b3c224a03d1d6507cc0ac74f9a993889da56d0
Developer-Mike <delta.cephei@outlook.com>
2025-08-22 10:11:41 -0400

    Solution 7, but slow

--- 7.reverse-integer.ts
+++ 7.reverse-integer.ts
@@ -7,17 +7,21 @@
 // @lc code=start
 function reverse(x: number): number {
   const sign = Math.sign(x)
+  if (x > -10 && x < 10) return x
   if (sign < 0) x = -x
+  
+  const digits = Math.ceil(Math.log10(x + 1))
 
-  const digits = Math.ceil(Math.log10(x))
   let reversed = 0
   for (let i = 0; i < digits; i++) {
     const oppositeOffsetDecimal = x / Math.pow(10, digits - i)
     const oppositeDecimal = oppositeOffsetDecimal - Math.floor(oppositeOffsetDecimal)
-    const oppositeDigit = Math.floor(oppositeDecimal * 10)
+    const oppositeDigit = Math.floor(oppositeDecimal * 10 + 1E-5 /* Rounding bug in JS :( */)
+    
     reversed += Math.pow(10, i) * oppositeDigit
   }
 
+  if (reversed > Math.pow(2, 31)) return 0
   return sign * reversed
 };
 // @lc code=end
